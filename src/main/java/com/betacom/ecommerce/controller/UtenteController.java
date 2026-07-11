c6e57a5ce6abec2642ce827d49d9b548f143498b
GabboLivrieri <gabriele.livrieri.005@gmail.com>
2026-07-11 09:09:16 +0200

    added execeptions

--- src/main/java/com/betacom/ecommerce/controller/UtenteController.java
+++ src/main/java/com/betacom/ecommerce/controller/UtenteController.java
@@ -1,10 +1,13 @@
 package com.betacom.ecommerce.controller;
 
+import com.betacom.ecommerce.dto.LoginDTO;
+import com.betacom.ecommerce.dto.UtenteRegistrazioneDTO;
+import com.betacom.ecommerce.dto.UtenteRispostaDTO;
+import com.betacom.ecommerce.exception.BadRequestException;
 import com.betacom.ecommerce.model.Utente;
 import com.betacom.ecommerce.repository.UtenteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/utenti")
@@ -13,22 +16,49 @@ public class UtenteController {
     @Autowired
     private UtenteRepository utenteRepository;
 
-    @GetMapping
-    public List<Utente> getAllUtenti() {
-        return utenteRepository.findAll();
-    }
-
     @PostMapping("/registrazione")
-    public Utente registraUtente(@RequestBody Utente utente) {
-        return utenteRepository.save(utente);
+    public UtenteRispostaDTO registraUtente(@RequestBody UtenteRegistrazioneDTO dto) {
+        
+        if (utenteRepository.findByEmail(dto.getEmail()) != null) {
+            throw new BadRequestException("Email già registrata nel sistema.");
+        }
+
+        Utente utente = new Utente();
+        utente.setNome(dto.getNome());
+        utente.setCognome(dto.getCognome());
+        utente.setEmail(dto.getEmail());
+        utente.setPassword(dto.getPassword());
+        utente.setRuolo(dto.getRuolo());
+
+        Utente salvato = utenteRepository.save(utente);
+
+        
+        UtenteRispostaDTO risposta = new UtenteRispostaDTO();
+        risposta.setId(salvato.getId());
+        risposta.setNome(salvato.getNome());
+        risposta.setCognome(salvato.getCognome());
+        risposta.setEmail(salvato.getEmail());
+        risposta.setRuolo(salvato.getRuolo());
+
+        return risposta;
     }
 
     @PostMapping("/login")
-    public Utente login(@RequestBody Utente credenziali) {
-        Utente u = utenteRepository.findByEmail(credenziali.getEmail());
-        if (u != null && u.getPassword().equals(credenziali.getPassword())) {
-            return u; 
+    public UtenteRispostaDTO login(@RequestBody LoginDTO dto) {
+        Utente u = utenteRepository.findByEmail(dto.getEmail());
+        
+        
+        if (u == null || !u.getPassword().equals(dto.getPassword())) {
+            throw new BadRequestException("Email o password non valide.");
         }
-        return null;
+
+        UtenteRispostaDTO risposta = new UtenteRispostaDTO();
+        risposta.setId(u.getId());
+        risposta.setNome(u.getNome());
+        risposta.setCognome(u.getCognome());
+        risposta.setEmail(u.getEmail());
+        risposta.setRuolo(u.getRuolo());
+
+        return risposta;
     }
 }
