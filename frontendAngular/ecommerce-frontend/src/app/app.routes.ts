d616bcf933d8e2a0ec74d7f6d3cf373c53c61d9e
GabboLivrieri <gabriele.livrieri.005@gmail.com>
2026-07-17 20:47:08 +0200

    created "i miei ordini" page for orders

--- frontendAngular/ecommerce-frontend/src/app/app.routes.ts
+++ frontendAngular/ecommerce-frontend/src/app/app.routes.ts
@@ -1,20 +1,22 @@
 import { Routes } from '@angular/router';
 import { Catalogo } from './pages/catalogo/catalogo';
-import { Home} from './pages/home/home';
+import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Registrazione } from './pages/registrazione/registrazione';
 import { Profilo } from './pages/profilo/profilo';
 import { authGuard } from './guards/auth.guard';
 import { Carrello } from './pages/carrello/carrello';
+import { Ordini } from './pages/ordini/ordini';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'catalogo', component: Catalogo },
-  { path: 'home', component: Home},
+  { path: 'home', component: Home },
   { path: 'login', component: Login },
   { path: 'registrazione', component: Registrazione },
-  { path: 'profilo', component: Profilo, canActivate: [authGuard]},
-  { path: 'carrello', component: Carrello, canActivate: [authGuard]}
+  { path: 'profilo', component: Profilo, canActivate: [authGuard] },
+  { path: 'carrello', component: Carrello, canActivate: [authGuard] },
+  { path: 'ordini', component: Ordini, canActivate: [authGuard] }
 
 ];
---
+++ frontendAngular/ecommerce-frontend/src/app/pages/ordini/ordini.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { OrdineService } from '../../services/ordine.service';
+import { Ordine } from '../../models/ordine.models';
+import { OrdineProdottoService } from '../../services/ordineProdotto.service';
+import { OrdineProdotto } from '../../models/ordineProdotto.models';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-ordini',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ordini.html',
+  styleUrl: './ordini.css'
+})
+export class Ordini implements OnInit {
+
+  ordini: Ordine[] = [];
+
+  prodottiOrdine: { [idOrdine: number]: OrdineProdotto[] } = {};
+
+
+  constructor(
+    private authService: AuthService,
+    private ordineService: OrdineService,
+    private ordineProdottoService: OrdineProdottoService,
+    private router: Router
+  ) {}
+
+
+  ngOnInit(): void {
+
+    const utente = this.authService.utenteCorrente;
+
+
+    if (!utente?.id) {
+
+      this.router.navigateByUrl('/login');
+      return;
+
+    }
+
+
+    this.ordineService
+      .getOrdiniUtente(utente.id)
+      .subscribe(ordini => {
+
+        this.ordini = ordini;
+
+
+        this.ordini.forEach(ordine => {
+
+          if (!ordine.id) {
+            return;
+          }
+
+
+          this.ordineProdottoService
+            .getProdottiOrdine(ordine.id)
+            .subscribe(prodotti => {
+
+              this.prodottiOrdine[ordine.id!] = prodotti;
+
+            });
+
+        });
+
+      });
+
+  }
+
+}
